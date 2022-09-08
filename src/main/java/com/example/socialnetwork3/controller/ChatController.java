8def853fd6fbf0d1c16ace66f3439cbce869e402
normallite1 <normallite@gmail.com>
2022-09-08 20:29:51 +0300

    add "Chat"

---
+++ src/main/java/com/example/socialnetwork3/controller/ChatController.java
@@ -0,0 +1,39 @@
+package com.example.socialnetwork3.controller;
+
+import com.example.socialnetwork3.model.ChatMessage;
+import com.example.socialnetwork3.model.User;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/ws")
+public class ChatController {
+    @GetMapping()
+    public String getChat(@AuthenticationPrincipal User userCurrent, Model model){
+        model.addAttribute("userCurrent", userCurrent);
+
+        return "chat";
+    }
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage,
+                               SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+}
